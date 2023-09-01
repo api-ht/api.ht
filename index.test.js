d81d219a528d42f0b52e34e4a4c3f5b68ea01d45
Tom Dolen <143192666+tomdolen@users.noreply.github.com>
2023-09-01 11:50:14 +0000

    Working through api interface & proxy

--- index.test.js
+++ index.test.js
@@ -1,8 +1,20 @@
 // import { describe, test, it, expect } from 'vitest'
-import { api } from './index.js'
+import { expect, test } from 'vitest'
+import { API, api } from './index.js'
 
 test('Math.sqrt()', () => {
   expect(Math.sqrt(4)).toBe(2)
   expect(Math.sqrt(144)).toBe(12)
   expect(Math.sqrt(2)).toBe(Math.SQRT2)
+})
+
+test('Schema Thing', async () => {
+  // const results = await API('ht').api.Thing({test: true})
+  const results = await API().schema.Thing()
+  expect(results.toBe({}))
+})
+
+test('IP Address', async () => {
+  const results = await api.ip('1.1.1.1')
+  expect(results.toBe({}))
 })
